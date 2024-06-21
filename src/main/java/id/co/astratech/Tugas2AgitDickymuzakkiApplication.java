ff9dac91b3b8dfe158654fadbae9b518dba4d194
student <hilmyarjuna@gmail.com>
2024-06-21 14:37:16 +0700

    tugas 2

---
+++ src/main/java/id/co/astratech/Tugas2AgitDickymuzakkiApplication.java
@@ -0,0 +1,13 @@
+package id.co.astratech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas2AgitDickymuzakkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tugas2AgitDickymuzakkiApplication.class, args);
+    }
+
+}
